3ce3d446a58d5806a1d4cdf76545d009fd01e22f
Cehdrian Espina <cehd0304@gmail.com>
2023-11-27 21:29:02 -0800

    Create Framework for bank class and main()

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+class Bank{
+    private:
+        int holder;
+    public:
+        int holder2;
+};
+
+int main(){
+    std::cout << "Check\n";
+    return 0;
+}
